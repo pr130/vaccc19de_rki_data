2d119ee339ae30dc07f6b8eaa82518260a80a5c5
Frie <fr1e@pm.me>
2020-12-31 14:31:19 +0100

    add notes to existing data, add maintenance scripts

---
+++ R/maintenance/update_cumulative_ts.R
@@ -0,0 +1,10 @@
+# add notes column for all datasets - as per vaccc19de 0.2.0
+library(vaccc19de)
+
+# all xlsx paths
+xlsxs <- list.files("data/raw/", pattern = ".xlsx$", full.names = T)
+
+cumulative_ts <- xlsxs %>%
+  purrr::map_dfr(function(path) {
+    rki_extract_cumulative_data(path, "data/raw")
+  })
