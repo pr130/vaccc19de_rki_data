82a4dc2591a9faf8caf802fbf9dc2850f0f94f58
Frie <fr1e@pm.me>
2021-01-02 10:43:12 +0100

    fix the download ts in data, delete kumulativ.csv

--- R/maintenance/update_cumulative_ts.R
+++ R/maintenance/update_cumulative_ts.R
@@ -6,5 +6,7 @@ xlsxs <- list.files("data/raw/", pattern = ".xlsx$", full.names = T)
 
 cumulative_ts <- xlsxs %>%
   purrr::map_dfr(function(path) {
-    rki_extract_cumulative_data(path, "data/raw")
+    rki_extract_cumulative_data(path)
   })
+
+readr::write_csv(cumulative_ts, "data/cumulative_time_series.csv")
