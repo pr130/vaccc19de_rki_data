cacd161a9870c0f6aba4403df73dbf85bbb00aac
Frie <fr1e@pm.me>
2021-01-02 15:21:30 +0100

    #2 need valid tag name even if skipped

--- R/download_and_update.R
+++ R/download_and_update.R
@@ -18,8 +18,8 @@ cumulative <- readr::read_csv("data/cumulative_time_series.csv")
 if (unique(rki_data$ts_datenstand) == max(cumulative$ts_datenstand)) {
   # no new data
   print(glue::glue("No new data. Skipping update."))
-  readr::write_lines("No update", "/tmp/ts_datenstand.txt")
-  readr::write_lines("No update", "/tmp/ts_download.txt")
+  readr::write_lines("no_update", "/tmp/ts_datenstand.txt")
+  readr::write_lines("no_update", "/tmp/ts_download.txt")
   fs::file_delete(c(xlsx_path, csv_paths))
   quit(status = 0, save = "no")
 }
